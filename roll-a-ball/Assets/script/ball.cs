92d35042f40f15506fc1e7df9293b104b2b088d8
gdmec07150805 <1395243186@qq.com>
2017-02-28 19:12:15 +0800

    demo

---
+++ roll-a-ball/Assets/script/ball.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour {
+
+    private int i=0;
+	// Update is called once per frame
+	void Update () {
+        float horizontal_move = Input.GetAxis("Horizontal");
+        float vertical_move = Input.GetAxis("Vertical");
+        this.GetComponent<Rigidbody>().AddForce(new Vector3(horizontal_move, 0, vertical_move)*100f);
+    }
+    void OnTriggerEnter(Collider other){
+        if (other.gameObject.name=="food"){
+            GameObject.Destroy(other.gameObject);
+            i++;
+            Debug.Log("Eat a food");
+            if (i == 10){
+                Debug.Log("game over");
+            }
+        }
+    }
+}
---
+++ roll-a-ball/Assets/script/followBall.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followBall : MonoBehaviour {
+
+    public GameObject ball;
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 ball_position = ball.GetComponent<Transform>().position;
+        this.GetComponent<Transform>().position =new Vector3(ball_position.x - 4.47f, ball_position.y+ 1.69f, ball_position.z-4.36f);
+    }
+}
